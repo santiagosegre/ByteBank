12fdf71998d4517d681580a7e9077416998fecbc
Santiago Segre <110129047+santiagosegre@users.noreply.github.com>
2023-01-10 15:19:09 -0300

    update

---
+++ ByteBank/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ByteBank {
+    public class Program {
+
+        static void ShowMenu() {
+            Console.WriteLine("1 - Inserir novo usuário");
+            Console.WriteLine("2 - Deletar um usuário");
+            Console.WriteLine("3 - Listar todas as contas registradas");
+            Console.WriteLine("4 - Detalhes de um usuário");
+            Console.WriteLine("5 - Quantia armazenada no banco");
+            Console.WriteLine("6 - Manipular a conta");
+            Console.WriteLine("0 - Para sair do programa");
+            Console.WriteLine("Digite a opção desejada: ");
+        }
+
+
+    }
+}
